35431a0798b64a086b52aeb01c31c6669770a15a
yangrui6927-prog <yangrui6927@gmail.com>
2026-03-17 21:30:35 +0800

    调整招标采购公告URL

--- bidding_notifier.py
+++ bidding_notifier.py
@@ -38,7 +38,7 @@ class BiddingScraper:
     """招标信息抓取器"""
     
     URLS = {
-        "招标采购公告": "https://b2b.10086.cn/#/biddingProcurementBulletin0",
+        "招标采购公告": "https://b2b.10086.cn/#/biddingProcurementBulletin",
         "采购服务": "https://b2b.10086.cn/#/procurementServices",
     }
     
